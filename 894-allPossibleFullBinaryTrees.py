22b6bc97c28845dd1ecee94fed2e7fc2bcd6ae1b
8jclxbl <451349242@qq.com>
2019-09-08 13:03:46 +0800

    20190908,1303

---
+++ 894-allPossibleFullBinaryTrees.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    memo = {0: [], 1: [TreeNode(0)]}
+
+    def allPossibleFBT(self, N):
+        if N not in Solution.memo:
+            ans = []
+            for x in range(N):
+                y = N - 1 - x
+                for left in self.allPossibleFBT(x):
+                    for right in self.allPossibleFBT(y):
+                        bns = TreeNode(0)
+                        bns.left = left
+                        bns.right = right
+                        ans.append(bns)
+            Solution.memo[N] = ans
+
+        return Solution.memo[N]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    res = s.allPossibleFBT(7)
+
+    print(res)
